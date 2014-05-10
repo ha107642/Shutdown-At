0ff8055484e8741ce21bc29f9d4d312f57529286
Henrik Alnestig <henrikalnestig@gmail.com>
2014-05-10 12:27:20 +0200

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ShutdownAt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.btnSchedule = new System.Windows.Forms.Button();
+            this.btnAbort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "When would you like to schedule a shutdown?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time:";
+            // 
+            // datePicker
+            // 
+            this.datePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datePicker.Location = new System.Drawing.Point(60, 36);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(127, 22);
+            this.datePicker.TabIndex = 3;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(60, 64);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(127, 22);
+            this.txtTime.TabIndex = 4;
+            // 
+            // btnSchedule
+            // 
+            this.btnSchedule.Location = new System.Drawing.Point(223, 99);
+            this.btnSchedule.Name = "btnSchedule";
+            this.btnSchedule.Size = new System.Drawing.Size(87, 27);
+            this.btnSchedule.TabIndex = 5;
+            this.btnSchedule.Text = "Schedule";
+            this.btnSchedule.UseVisualStyleBackColor = true;
+            this.btnSchedule.Click += new System.EventHandler(this.btnSchedule_Click);
+            // 
+            // btnAbort
+            // 
+            this.btnAbort.Location = new System.Drawing.Point(142, 99);
+            this.btnAbort.Name = "btnAbort";
+            this.btnAbort.Size = new System.Drawing.Size(75, 27);
+            this.btnAbort.TabIndex = 6;
+            this.btnAbort.Text = "Abort";
+            this.btnAbort.UseVisualStyleBackColor = true;
+            this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 138);
+            this.Controls.Add(this.btnAbort);
+            this.Controls.Add(this.btnSchedule);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.datePicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Shutdown At";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnSchedule;
+        private System.Windows.Forms.Button btnAbort;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutdownAt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            DateTime date = DateTime.Today.Date;
+            txtTime.Text = time.Hours.ToString().PadLeft(2, '0') + ":" + time.Minutes.ToString().PadLeft(2, '0');
+            datePicker.Text = date.ToString();
+        }
+
+        private void btnSchedule_Click(object sender, EventArgs e)
+        {
+            String time = txtTime.Text;
+            String date = datePicker.Text;
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = "schtasks";
+            processInfo.Arguments = "/create /tn \"Shutdown\" /tr \"shutdown -s\" /sc once /st " + time + " /sd " + date + " /F";
+            processInfo.UseShellExecute = true;
+            processInfo.Verb = "runas"; //Run as administrator.
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+            if (process.ExitCode == 0)
+                MessageBox.Show("The computer will shut down at\n" + date + " " + time);
+            else
+                MessageBox.Show("Error. " + process.ExitCode);
+        }
+
+        private void btnAbort_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = "schtasks";
+            processInfo.Arguments = "/delete /tn \"Shutdown\" /f";
+            processInfo.UseShellExecute = true;
+            processInfo.Verb = "runas"; //Run as administrator.
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+            if (process.ExitCode == 0)
+                MessageBox.Show("The scheduled shutdown has been aborted");
+            else
+                MessageBox.Show("There was no scheduled shutdown to abort");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
